87105127bd31da203b02ac0cf93aec94b6602210
Adam <otkb776@gmail.com>
2016-10-26 11:03:25 -0500

    I am done.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This code creates an action to take a matrix and create an inverse of it.
+#If that inverse has already been saved into the environment,
+#The second function will call the cached version.
 
-## Write a short comment describing this function
+## Creates a set of functions that allow the next function to scope them
+## and check for the cache of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matinv <- NULL
+  set <- function(v){
+    x <<- v
+    matinv <<- NULL
+  }
+  matx <- function() x
+  setinv <- function(invmat) matinv <<- invmat
+  invmatx <- function() matinv
+  list(set = set, matx = matx, setinv = setinv, invmatx = invmatx)
 }
 
 
-## Write a short comment describing this function
+## Either pulls a cached version of the inverse matrix or creates the 
+## inverse matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matinv <- x$invmatx()
+  if(!is.null(matinv)) {
+    message("Retrieving cached data.")
+    return(matinv)
+  }
+  matx <- x$matx()
+  matinv <- solve(matx)
+  x$setinv(matinv)
+  matinv
+  ## Return a matrix that is the inverse of 'x'
 }
